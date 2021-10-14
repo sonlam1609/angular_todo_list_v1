b57d83f8aa44e1a6e660dc03fb787681509281b3
SonNguyen <nguyenlamson1609@gmail.com>
2021-10-14 09:02:32 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list-v1';
+  input = '';
+
+  todoList = [
+    { id: 1, name: 'To do 1', status: true },
+    { id: 2, name: 'To do 2', status: false },
+    { id: 3, name: 'To do 3', status: true },
+  ];
+
+  addTodo() {
+    if (!this.input.length) {
+      alert('Please enter input');
+    } else {
+      this.todoList.push({
+        id: this.todoList[this.todoList.length - 1].id + 1,
+        name: this.input,
+        status: false,
+      });
+      this.input = '';
+    }
+  }
+
+  removeTodo(id: number) {
+    const index = this.todoList.findIndex((t) => t.id === id);
+    this.todoList.splice(index, 1);
+  }
+
+  changeStatus(item: any) {
+    item.status = !item.status;
+  }
 }
